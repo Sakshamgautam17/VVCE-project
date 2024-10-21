f897595c18fc0cc46b7050e14a9e2c53d0cc31f7
ArshBakshi <arshbakshi@gmail.com>
2024-10-22 00:30:56 +0530

    asdf

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("HIII!");
-});
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
